f76f76b92c5d5521417f9c8f490239ad3e18aa29
gearhead0621 <53133728+gearhead0621@users.noreply.github.com>
2021-02-08 23:23:06 -0500

    Adding initial software build

---
+++ ShapeViewerLibrary/ShapeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeViewerLibrary
+{
+    public class ShapeModel
+    {
+        public List<Shape> Shapes { get; set; } = new List<Shape>();
+        // Might not need the pub string
+        public string ShapeName { get; set; }
+    }
+}
