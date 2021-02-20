ec397a494b58c79eadc169834413c024d1314313
Nick Szkodyn <nszkodyn@gmail.com>
2021-02-20 17:54:01 -0500

    Torched and rewrote shapes classes

--- ShapeUI/Shape.cs
+++ ShapeUI/Shape.cs
@@ -3,40 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ShapeUI
 {
+    public class Point
+    {
+        public string Type { get; set; }
+        public double Value { get; set; }
+
+        public Point()
+        {
+            this.Type = $"t";
+                .Value = $"v";
+        }
+    }
+    
     public class Shape
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Point1Type { get; set; }
-        public int Point1Value { get; set; }
-        public string Point2Type { get; set; }
-        public int Point2Value { get; set; }
-        public string Point3Type { get; set; }
-        public int Point3Value { get; set; }
-        public string Point4Type { get; set; }
+        public string[] Points { get; set; } // Not sure if this is correct
         
-        public string Point5Type { get; set; }
-        
-        public string Point6Type { get; set; }
-        
-        public string Point7Type { get; set; }
+        // TODO - Add color property
 
-
-        public static Shape ParseFromCsv<TResult>(string line)
+        public Shape()
         {
-            var columns = line.Split(',');
-
-            return new Shape
-            {
-                Id = int.Parse(columns[0]),
-                Name = columns[1],
-                Point1Type = columns[2],
-                Point1Value = int.Parse(columns[3]),
-                Point2Type = columns[4],
-            };
+            this.Id = { }
+                .Name = "";
+                .Points = null;
         }
+
+        Shape(int id, string name, string[] points)
+        {
+            // Add something here...
+        }
+
+        // virtual Draw() method
+        // virtual Area() method
+        // virtual Perimeter() method
+        // GetAvailableShapes()
+    }
+
+    public class Polygon : Shape
+    {
+        public void Draw
+        {
+            // Implement Draw()
+        }
+
+        public float GetArea()
+        {
+            // Implement GetArea()
+            return 0;
+        }
+
+        public float GetPerimeter()
+        {
+            // Implement GetPerimeter()
+            return 0;
+        }
+    }
+
+    public class Ellipse : Shape
+    {
+
     }
 }
