8621e09e2326ec0d7f3c9c65c458bf0a7990ed6e
Nick Szkodyn <nszkodyn@gmail.com>
2021-02-20 21:42:51 -0500

    More shapes classes updates

--- ShapeUI/Shape.cs
+++ ShapeUI/Shape.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace ShapeUI
 {
@@ -67,6 +70,54 @@ namespace ShapeUI
 
     public class Ellipse : Shape
     {
+        public void Draw
+        {
+            // Implement Draw()
+        }
+
+        public float GetArea()
+        {
+            // Implement GetArea()
+            return 0;
+        }
+
+        public float GetPerimeter()
+        {
+            // Implement GetPerimeter()
+            return 0;
+        }
+    }
+
+    public class ShapeFactory
+    {
+        public Shape[] Shapes();
+
+        ShapeFactory()
+        {
+
+            // Fetch the raw data
+            public string Path = "PSScriptRoot" + "\\ShapeList.csv";
+            public string RawData = import - csv @"C:\Github Repo\Shape_Viewer\ShapeUI\ShapeList.csv";
+
+            // RawData format: ID | Name | Points[X]
+            // Points[X] = (pointXType,pointXValue)
+
+            //public string tmpShapes = foreach (var object in public string RawData1 => RawData)
+            //{
+                //public string tmp = [PSCustomObject]::new()
+                //string properties = object | Get-Member -MemberTypes Properties | Select -ExpandProperty Name // Pulls just the header names
+                //foreach (var property in string Properties)
+                //{
+                    //if (string object.property.length != 0)
+                        //{
+                            //tmp | Add-Member -NotePropertyName string property -NotePropertyValue string object.property
+                        //}
+                //}
+
+
+            //}
+
+        }
 
     }
 }
